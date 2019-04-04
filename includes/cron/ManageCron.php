ef9e21355921c7e91ba8175ef14143ac28004c47
Fabian Horlacher <mail@horlacher.be>
2019-04-04 17:33:13 +0200

    CronMailIndex: also track is_step2_done

--- includes/cron/ManageCron.php
+++ includes/cron/ManageCron.php
@@ -25,7 +25,7 @@ class ManageCron
 	/**
 	 * @var $crons array
 	 */
-	static private $crons = ['CronMailConfirm', 'CronMailIndex', 'CronMailRemind',];
+	static private $crons = ['CronMailConfirm', 'CronMailIndex', 'CronMailRemindSheet',];
 
 	public function __construct($pluginName, $version)
 	{
