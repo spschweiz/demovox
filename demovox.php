372c250190b46308cf2903c8fd949a6baf52a9aa
Jonas Zürcher <jonas.zuercher@spschweiz.ch>
2019-04-05 19:19:19 +0200

    Update demovox.php

--- demovox.php
+++ demovox.php
@@ -18,7 +18,7 @@ namespace Demovox;
  * Plugin Name:       demovox
  * Plugin URI:        https://demovox.ch/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Fabian Horlacher / SP Schweiz
  * Author URI:        https://www.spschweiz.ch/
  * GitHub Plugin URI: https://github.com/spschweiz/demovox
@@ -38,7 +38,7 @@ if (!defined('WPINC')) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define('DEMOVOX_VERSION', '1.0.0');
+define('DEMOVOX_VERSION', '1.0.1');
 
 /**
  * The code that runs during plugin activation.
